9d1c1d01782b8f1642b4cbb468c985a845a9359c
Jose Pena <yadose@gmail.com>
2017-06-16 17:03:03 +0200

    added rest api class

---
+++ src/classes/MonitoringToolAPI.php
@@ -0,0 +1,35 @@
+<?php
+namespace Classes;
+use \Mysqli;
+//use \PDO;
+class MonitoringToolAPI {
+  //DATABASE
+  private $database;
+  private $databasehost;
+  private $databaseuser;
+  private $databasepassword;
+  public function __construct($dbname,$dbhost,$dbuser,$dbpasswd){
+    $this->database = $dbname;
+    $this->databasehost = $dbhost;
+    $this->databaseuser = $dbuser;
+    $this->databasepassword = $dbpasswd;
+  }
+  public function connect(){
+    //phpinfo();
+    $mysqli = new Mysqli($this->databasehost,$this->databaseuser,$this->databasepassword,$this->database);
+    //$pdo = new PDO("mysql:host=" . $this->databasehost . ";dbname=" . $this->database, $this->databaseuser, $this->databasepassword);
+    //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    if($mysqli->connect_errno){
+      echo "Errno: " . $mysqli->connect_errno . "\n";
+      echo "Error: " . $mysqli->connect_error . "\n";
+    }
+    //die('');
+  }
+  public function finishedJob(){
+
+  }
+  public function reportError(){
+
+  }
+}
