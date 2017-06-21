50910bae239f111c80f064e87cc0cf7a5d2efdd3
Jose Pena <yadose@gmail.com>
2017-06-21 10:01:14 +0200

    working example fetching username from db

--- src/public/index.php
+++ src/public/index.php
@@ -7,13 +7,14 @@ require '../vendor/autoload.php';
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
-$config['db']['host'] = "82.165.160.231";
-$config['db']['user'] = "joseuser";
-$config['db']['pass'] = "Mld7_c28%";
+$config['db']['host'] = "localhost";
+$config['db']['user'] = "root";
+$config['db']['pass'] = "jose8819";
 $config['db']['dbname'] = "josetest";
 
+
+
 $app = new \Slim\App(["settings" => $config]);
-//$db = new \Classes\MonitoringToolAPI($config['db']['dbname'],$config['db']['host'],$config['db']['user'],$config['db']['pass']);
 
 $container = $app->getContainer();
 //add logger
@@ -24,14 +25,6 @@ $container['logger'] = function($c) {
     return $logger;
 };
 
-//add database connection
-/*$container['db'] = function ($c) {
-    $db = $c['settings']['db'];
-    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'], $db['user'], $db['pass']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    return $pdo;
-};*/
 $container['db'] = function($c){
   $db = $c['settings']['db'];
   $api = new \Classes\MonitoringToolAPI($db['dbname'],$db['host'],$db['user'],$db['pass']);
@@ -42,8 +35,8 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
 
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
-    print_r($this->db);
-    $this->logger->addInfo('Some info');
+    $this->db;
+    //$this->logger->addInfo('Some info');
     return $response;
 });
 
